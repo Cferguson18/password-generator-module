733a14ccd5c1e3340aeb34a8772c6472334020f6
Cferguson18 <127478897+Cferguson18@users.noreply.github.com>
2023-04-06 13:55:10 -0700

    updated

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 function writePassword() {
   
  var password = document.getElementById("#password");
+ var passwordText = document.querySelector("#password");
   var chars = "";
   //prompts//
   passwordLength = prompt("Pick password length between 8-128");
@@ -25,32 +26,36 @@ function writePassword() {
   numbers = prompt("would you would like numbers (yes/no)");
   symbols = prompt("would you would like special characters (yes/no");
 
-  // 1 choice//
-  if(uppercase === "yes") {chars += upper;}
-  else if(lowercase === "yes") {chars += lower;}
-  else if(symbols === "yes") {chars += sym;}
-  else if(numeric === "yes") {chars += number;}
-  //all 2 choices/
-  else if(lowercase && uppercase === "yes") {chars += lower && upper;}
-  else if(lowercase && symbols === "yes") {chars += lower && sym;}
-  else if(symbols && numeric === "yes") {chars += sym && numbers;}  
-  else if(symbols && uppercase === "yes") {chars += sym && upper;}
-  else if(lowercase && numeric === "yes") {chars += lower && number;}
-  else if(numeric && uppercase === "yes") {chars += numbers && upper;}
-   //all 3 choices//
-   else if(numeric && lowercase && uppercase === "yes") {chars += numbers && lower && upper;}
-   else if(numeric && uppercase && symbols === "yes") {chars += numbers && sym && upper;}
-   else if(numeric && lowercase && symbols === "yes") {chars += numbers && lower && sym;}
-   else if(symbols && lowercase && uppercase === "yes") {chars += symbols && lower && upper;}
 //all 4 choices//
-   else if (symbols && numeric && lowercase && uppercase=== "yes") { chars += sym && number && lower && upper;}
+   if (symbols && numeric && lowercase && uppercase == "yes") { chars += sym && number && lower && upper;}
+   //all 3 choices//
+   else if(numeric && lowercase && uppercase == "yes") {chars += numbers && lower && upper;}
+   else if(numeric && uppercase && symbols == "yes") {chars += numbers && sym && upper;}
+   else if(numeric && lowercase && symbols == "yes") {chars += numbers && lower && sym;}
+   else if(symbols && lowercase && uppercase == "yes") {chars += symbols && lower && upper;}
+   //all 2 choices/
+  else if(lowercase && uppercase == "yes") {chars += lower && upper;}
+  else if(lowercase && symbols == "yes") {chars += lower && sym;}
+  else if(symbols && numeric == "yes") {chars += sym && numbers;}  
+  else if(symbols && uppercase == "yes") {chars += sym && upper;}
+  else if(lowercase && numeric == "yes") {chars += lower && number;}
+  else if(numeric && uppercase == "yes") {chars += numbers && upper;}
+   // 1 choice//
+   else if(uppercase == "yes") {chars += upper;}
+   else if(lowercase == "yes") {chars += lower;}
+   else if(symbols == "yes") {chars += sym;}
+   else if(numeric == "yes") {chars += number;}
 //generate passcode with answers//
   var password ="";
-
+  
   for(var i= 1; i<= passwordLength; i++){
     var randomPassword = Math.floor(Math.random() * chars.length);
     password += chars.substring(randomPassword, randomPassword +1);
-  }
+  
+  passwordText.value = password;
+
+}
+//display//
 document.getElementById("password").value = password;
  }
 
